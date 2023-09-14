b58dbe8075226d2f9adf3ae404dc2975d2ab5aed
apm30 <m.ajaypradeep@icloud.com>
2023-09-14 18:53:27 +0100

    Adding Procfile for Heroku and updating random.js

--- routes/random.js
+++ routes/random.js
@@ -1,37 +1,37 @@
 const express = require("express");
-
-const Riddle = require("../model/riddle");
-const router = express.Router();
 const bodyParser = require("body-parser");
-router.use(bodyParser.urlencoded({ extended: false }));
+const Riddle = require("../model/riddle");
 
-router.use(express.json());
-
+const router = express.Router();
+const MAX_RIDDLES = 101;
 const numbers = [];
 
-router.get("/", (req, res) => {
-  Riddle.aggregate([{ $sample: { size: 1 } }])
-    .then((result) => {
-      console.log(result);
-      const number = result[0].No;
-      if (numbers.includes(number)) {
-        // Total number of riddles is 101 so if the array length is 101, it means that all the riddles have been displayed once , adding more riddles means you have to handle the case when the array length is 101.
-        if (numbers.length === 101) {
-          // Resetting the array
-          numbers.splice(0, 100);
-          console.log("The array has been reset");
-        }
-        res.redirect("/random");
-        console.log("This number is already in the array");
-      } else {
-        numbers.push(number);
-        console.log(numbers);
-        res.json(result);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Riddle.aggregate([{ $sample: { size: 1 } }]);
+    const number = result[0].No;
+
+    if (numbers.length === MAX_RIDDLES) {
+      numbers.splice(0, MAX_RIDDLES - 1);
+      console.log("The array has been reset");
+      return res.redirect("/random");
+    }
+
+    if (numbers.includes(number)) {
+      console.log("This number is already in the array");
+      return res.redirect("/random");
+    }
+
+    numbers.push(number);
+    console.log(numbers);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
