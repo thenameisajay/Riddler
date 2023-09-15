bf168110ec855135810f74555c39efffed0cac56
apm30 <m.ajaypradeep@icloud.com>
2023-09-15 16:10:48 +0100

    port typo

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 
